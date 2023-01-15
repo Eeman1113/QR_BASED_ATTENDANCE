d4edccd376994d575faa143858a975211f632275
Eeman Majumder <eemanmajumder@Eemans-MacBook-Pro.local>
2023-01-15 23:50:02 +0530

    anal

---
+++ Python/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+import pyqrcode
+import png
+from pyqrcode import QRCode
+import random
+
+def qr_gen(s):
+    url = pyqrcode.create(s)
+    url.svg("/Users/eemanmajumder/code_shit/QR_BASED_ATTENDANCE/QR/{}.svg".format(s), scale = 8)
+    url.png('/Users/eemanmajumder/code_shit/QR_BASED_ATTENDANCE/QR/{}.png'.format(s), scale = 6)
+a=input('enter event name')
+if a=='TAB':
+    df=pd.read_csv('TAB.csv','w')
+    df=df.dropna()
+    #add a coloumn to the csv file with random string
+    df['Unique_ID']=df['Name'].apply(lambda x: ''.join(random.choice('0123456789ABCDEF') for i in range(16)))
+    for i in df['Unique_ID']:
+        qr_gen(i)
+elif a=='AB':
+    df=pd.read_csv('AB.csv','w')
+    df=df.dropna()
+    #add a coloumn to the csv file with random string
+    df['Unique_ID']=df['Name'].apply(lambda x: ''.join(random.choice('0123456789ABCDEF') for i in range(16)))
+    for i in df['Unique_ID']:
+        qr_gen(i)
+elif a=="C":
+    df=pd.read_csv('C.csv','w')
+    df=df.dropna()
+    #add a coloumn to the csv file with random string
+    df['Unique_ID']=df['Name'].apply(lambda x: ''.join(random.choice('0123456789ABCDEF') for i in range(16)))
+    for i in df['Unique_ID']:
+        qr_gen(i)
+
+else:
+    print("Invalid Event Name")
+
+# s = "congratulations! sakshiiiii"
+# url = pyqrcode.create(s)
+# url.svg("/Users/eemanmajumder/code_shit/QR_BASED_ATTENDANCE/QR/myqr.svg", scale = 8)
+# url.png('/Users/eemanmajumder/code_shit/QR_BASED_ATTENDANCE/QR/myqr.png', scale = 6)
